0dd69e099194dd9af6b434fbe46e2ae5d185d9ec
TheFive666 <116151187+TheFive666@users.noreply.github.com>
2024-08-09 14:27:44 +0800

    Update Robot_test.py

--- 源码/调试工具/Robot_test.py
+++ 源码/调试工具/Robot_test.py
@@ -6,7 +6,7 @@ robot2 = ultraArm("COM9")
 def check_robot(ua):
     ua.go_zero()
     ua.sync()
-    ua.set_angle(4,90，100)
+    ua.set_angle(4,90,100)
     ua.sync()
     ua.set_gpio_state(0)
     time.sleep(2)
