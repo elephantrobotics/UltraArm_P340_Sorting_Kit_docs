7d4c3a9198d09b90a37204bfef0c505f3e61e4f0
TheFive666 <116151187+TheFive666@users.noreply.github.com>
2024-08-09 14:28:35 +0800

    Update Conveyor_test.py

--- 源码/调试工具/Conveyor_test.py
+++ 源码/调试工具/Conveyor_test.py
@@ -5,8 +5,12 @@ sys.path.append(BASE_DIR)
 from ConveyorMain import *
 conveyer=ConveyorMain("COM5")
 
-for i in range(2):
-     conveyer.open_conveyor(100)
-     time.sleep(5)
-     conveyer.close_conveyor()
-     time.sleep(2)
+for i in range(100):
+     try:
+          conveyer.open_conveyor(100)
+          time.sleep(5)
+          conveyer.close_conveyor()
+          time.sleep(2)
+     #按住键盘CTRL+C键，可让传送带停止
+     except KeyboardInterrupt:
+          conveyer.close_conveyor()
