f53a681d4f66335564d9ea97a1cb523f679941fe
TheFive666 <116151187+TheFive666@users.noreply.github.com>
2025-08-06 13:38:07 +0800

    Update demo.py

--- source_code/demo.py
+++ source_code/demo.py
@@ -13,6 +13,7 @@ if __name__=="__main__":
     robot=Vision_Unstacking_Robot("COM12",robot_speed=150)
     robot2=Palletizing_Robot("COM8",robot_speed=150,x_shift=10,y_shift=10)
     conveyer=ConveyorMain("COM10")
+    time.sleep(2)
     conveyer.open_conveyor(100)
             
     time.sleep(10)
